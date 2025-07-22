0cbdf1942ed959e966a0b534b6cc90602a3e35dd
Amjko <ianagustin.connect@gmail.com>
2025-07-22 16:07:17 +0800

    improve: changed to class that also catches uncaught exception

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,38 @@
 import logging
+import sys
+from typing import Optional
 
-# Logger
-HANDLER = logging.FileHandler(filename="logs/discord.log", encoding="utf-8", mode="w")
 
-# For detailed logs
-HANDLER_LEVEL = logging.DEBUG
+class Logger:
+    _instance: Optional["Logger"] = None
+
+    def __init__(self):
+        self.handler = logging.FileHandler(
+            filename="logs/discord.log", encoding="utf-8", mode="w"
+        )
+        self.handler_level = logging.DEBUG
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def run(self):
+        logging.basicConfig(
+            level=self.handler_level,
+            handlers=[self.handler],
+            format="%(asctime)s:%(levelname)s:%(name)s: %(message)s",
+        )
+
+        # Custom uncaught exception catcher
+        def handle_exception(except_type, except_value, except_traceback):
+            if issubclass(except_type, KeyboardInterrupt):
+                sys.__excepthook__(except_type, except_value, except_traceback)
+                return
+            logging.critical(
+                "Uncaught exception",
+                exc_info=(except_type, except_value, except_traceback),
+            )
+
+        # Override the catcher with custom catcher
+        sys.excepthook = handle_exception
